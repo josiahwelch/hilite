338f4f2e6c5dd8208039ebb025cfb8fcea180f69
Josiah Welch <josiah.isaiah.welch@gmail.com>
2025-01-13 21:39:46 -0600

    fixed hilite.c; the program will highlight properly and will take color inputs properly... still needs piping support

--- src/hilite.c
+++ src/hilite.c
@@ -15,6 +15,48 @@ char *setTextColor(char *text, int color) {
 	return &buffer;
 }
 
+int isStringEqual(char *str1, char *str2) {
+
+	//returns if two strings are identical... strcmp() is giving me segmentation faults and I'm too lazy to deal with it
+	
+	if(strlen(str1) != strlen(str2)) { return 0; } //handles the easiest subproblem... if the two strings are different sizes then the two strings obviously cannot be identical
+	
+	for(int i = 0; i < strlen(str1); i++) {
+
+		if(str1[i] != str2[i]) { return 0; }
+	
+	}
+
+	return 1;
+
+}
+
+int isDigit(char *str) {
+
+	//returns if a string contains an integer value... isdigit is also giving me segmentation faults and I'm too lazy to deal with it
+	
+	char digits[] = "1234567890";
+
+	int isCurrentCharDigit = 0;
+
+	for(int i = 0; i < strlen(str); i++) {
+
+		isCurrentCharDigit = 0;
+
+		for(int indexOfDigits = 0; indexOfDigits < 10; indexOfDigits++) {
+
+			if(str[i] == digits[indexOfDigits]) { isCurrentCharDigit = 1; }
+
+		}
+
+		if(!isCurrentCharDigit) { return 0; }
+
+	}
+
+	return 1;
+
+}
+
 int IntegerParse(char *charsToParse) {
 
 	//returns a parsed integer from a char array with ASCII support
@@ -27,7 +69,7 @@ int IntegerParse(char *charsToParse) {
 
 		index = strlen(charsToParse) - 1 - i; //sets the index of the char being parsed
 
-		parsedInt += (charsToParse[index] - 48) * pow(10, i); //adds the char from the char array with ASCII conversion and decimal parsing
+		parsedInt += (charsToParse[index] - 48) * (int) pow(10.0, (double) i); //adds the char from the char array with ASCII conversion and decimal parsing
 
 	}
 
@@ -41,45 +83,45 @@ int main(int argc, char **argv) {
 	int color;
 	char *textToFind;
 	char *text;
-
+	
 	//parses arguments
-	if(argc = 5) {
+	if(argc == 5) {
 
 		//handles background color
-		if(strcmp(argv[1], "-b") || strcmp(argv[1], "--background")) {
+		if(isStringEqual(argv[1], "-b") || isStringEqual(argv[1], "--background")) {
 
-			if(strcmp(argv[2], "black")) { color = 40; }
+			if(isStringEqual(argv[2], "black")) { color = 40; }
 
-			if(strcmp(argv[2], "red")) { color = 41; }
+			if(isStringEqual(argv[2], "red")) { color = 41; }
 
-			if(strcmp(argv[2], "green")) { color = 42; }
+			if(isStringEqual(argv[2], "green")) { color = 42; }
 
-			if(strcmp(argv[2], "yellow")) { color = 43; }
+			if(isStringEqual(argv[2], "yellow")) { color = 43; }
 
-			if(strcmp(argv[2], "blue")) { color = 44; }
+			if(isStringEqual(argv[2], "blue")) { color = 44; }
 
-			if(strcmp(argv[2], "white")) { color = 47; }
+			if(isStringEqual(argv[2], "white")) { color = 47; }
 
-			if(isdigit(argv[3])) { color = IntegerParse(argv[3]); }
+			if(isDigit(argv[2])) { color = IntegerParse(argv[2]); }
 
 		}	
 
 		//handles foreground color
-		if(strcmp(argv[1], "-f") || strcmp(argv[1], "--foreground")) {
+		if(isStringEqual(argv[1], "-f") || isStringEqual(argv[1], "--foreground")) {
 
-			if(strcmp(argv[2], "black")) { color = 30; }
+			if(isStringEqual(argv[2], "black")) { color = 30; }
 
-			if(strcmp(argv[2], "red")) { color = 31; }
+			if(isStringEqual(argv[2], "red")) { color = 31; }
 
-			if(strcmp(argv[2], "green")) { color = 32; }
+			if(isStringEqual(argv[2], "green")) { color = 32; }
 
-			if(strcmp(argv[2], "yellow")) { color = 33; }
+			if(isStringEqual(argv[2], "yellow")) { color = 33; }
 
-			if(strcmp(argv[2], "blue")) { color = 34; }
+			if(isStringEqual(argv[2], "blue")) { color = 34; }
 
-			if(strcmp(argv[2], "white")) { color = 37; }
+			if(isStringEqual(argv[2], "white")) { color = 37; }
 
-			if(isdigit(argv[3])) { color = IntegerParse(argv[3]); }
+			if(isDigit(argv[2])) { color = IntegerParse(argv[2]); }
 
 		}	
 
@@ -88,16 +130,17 @@ int main(int argc, char **argv) {
 
 	}
 
-	else if(argc = 3) {
-		color = 43; //yellow background
+	else if(argc == 3) {
+		color = 103; //yellow background
 
-		textToFind = argv[2];
-		text = argv[3];
+		textToFind = argv[1];
+		text = argv[2];
+
 	}
 
-	else if(argc = 2 && (strcmp(argv[1], "-h") || strcmp(argv[1], "--help"))) {
+	else if(argc == 2 && (isStringEqual(argv[1], "-h") || isStringEqual(argv[1], "--help"))) {
 
-		printf("hilite [OPTIONS] TEXT_TO_HIGHLIGHT TEXT_TO_BE_HIGHLIGHTED\n\nOPTIONS:\n-h/--help\t\tProvides help with usage\n-b/--background {COLOR | COLOR_CODE}\t\tSets the background color to user specification\n-f/--foreground\t\tSets the color of the highlighted text\n\nCOLORS:\nblack\nred\ngreen\nyellow\nblue\nwhite\n\nCOLOR_CODE: ANSI escape color codes... such as 35 for magenta-colored text\n");
+		printf("hilite [OPTIONS] TEXT_TO_HIGHLIGHT TEXT_TO_BE_HIGHLIGHTED\n\nOPTIONS:\n-h/--help\t\t\t\t\tProvides help with usage\n-b/--background {COLOR | COLOR_CODE}\t\tSets the background color to user specification\n-f/--foreground {COLOR | COLOR_CODE}\t\tSets the color of the highlighted text\n\nCOLORS:\nblack\nred\ngreen\nyellow\nblue\nwhite\n\nCOLOR_CODE: ANSI escape color codes... such as 35 for magenta-colored text\n");
 
 		return 0;
 	}
@@ -116,15 +159,23 @@ int main(int argc, char **argv) {
 
 	else {
 
-		while(ptr != NULL) {
+		while(ptr != NULL && ptr < (text + strlen(text))) {
 
-			for(int i = 0; i < (ptr - text); i++) { printf(text[i]); } //prints text all the way up to where textToFind was found
+			for(int i = 0; i < (ptr - text); i++) { printf("%c", text[i]); } //prints text all the way up to where textToFind was found
+
+			//printf("%s : %s\n", text, ptr);
 			
-			text = (ptr + strlen(textToFind));
+			ptr += strlen(textToFind);
+			
+			text = ptr; //sets the pointer pointing to the location of the text to be after where the instance of textToFind was found
 
-			printf(setTextColor(textToFind, color));
+			printf("%s", setTextColor(textToFind, color));
+
+			ptr = strstr(text, textToFind);
 
 		}
+
+		printf("%s\n", text);
 
 	}
 
